f4447d0397936d23d9277f58cdc1811b75e901cf
Dmitry Osipov <dmith.osipov@gmail.com>
2022-02-15 15:56:23 +0300

    add get functions names and paramters

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,6 @@ pub unsafe extern "C" fn handle() {
     match input.first().unwrap() {
         &0 => {
             let v: Vec<u8> = input[1..].to_vec();
-            // let message = MessageIn::decode(&mut v.as_slice()).unwrap();
             STATE.push(MessageIn::decode(&mut v.as_slice()).unwrap())
         }
         &1 => {
@@ -32,28 +31,6 @@ pub unsafe extern "C" fn handle() {
 #[no_mangle]
 pub unsafe extern "C" fn init() {}
 
-// #[wasm_bindgen]
-// pub enum Functions {
-//     AddMessage = 0,
-//     ViewMessages = 1,
-// }
-
-// #[wasm_bindgen]
-// pub fn handle_encode(func: Functions, author: String, msg: String) -> Vec<u8> {
-//     let mut v: Vec<u8> = Vec::new();
-//     match func {
-//         Functions::AddMessage => {
-//             v.push(0);
-//             let mut m = MessageIn { author, msg }.encode();
-//             v.append(&mut m);
-//         }
-//         Functions::ViewMessages => {
-//             v.push(1);
-//         }
-//     };
-//     v
-// }
-
 #[wasm_bindgen]
 pub fn add_message(author: String, msg: String) -> Vec<u8> {
     let mut v: Vec<u8> = Vec::new();
@@ -64,7 +41,7 @@ pub fn add_message(author: String, msg: String) -> Vec<u8> {
 }
 
 #[wasm_bindgen]
-pub fn view_message() -> Vec<u8> {
+pub fn view_messages() -> Vec<u8> {
     let mut v: Vec<u8> = Vec::new();
     v.push(1);
     v
--- test.js
+++ test.js
@@ -1,14 +1,35 @@
 const { u8aToHex } = require('@polkadot/util');
+const { strictEqual, deepEqual } = require('assert');
+
 const rust = import('./pkg/bindgen_guestbook.js');
-const { strict } = require('assert');
+
+/**
+ *
+ * @param {Function} func
+ */
+function getFunctionParams(func) {
+  const stringFunc = func.toString();
+  const startI = stringFunc.indexOf('(') + 1;
+  const endI = stringFunc.indexOf(')');
+  const sliced = stringFunc.slice(startI, endI);
+  return sliced.length > 0 ? sliced.split(',').map((value) => value.trim()) : null;
+}
+
+function getFunctionsNames(module) {
+  return Object.keys(module).filter((key) => !['default', '__wasm'].includes(key));
+}
 
 rust.then((m) => {
-  strict.strictEqual(
-    u8aToHex(m.add_message('author', 'message')),
-    '0x0018617574686f721c6d657373616765',
-    'add_message result is wrong',
-  );
+  deepEqual(getFunctionsNames(m), ['add_message', 'view_messages'], 'get functions names ❌');
+  console.log('get functions names ✅');
+
+  deepEqual(getFunctionParams(m.add_message), ['author', 'msg'], 'get add_message params ❌');
+  deepEqual(getFunctionParams(m.view_messages), null, 'get view_messages params ❌');
+  console.log('get function params ✅');
+
+  strictEqual(u8aToHex(m.add_message('author', 'message')), '0x0018617574686f721c6d657373616765', 'add_message ❌');
   console.log('add_message ✅');
-  strict.strictEqual(u8aToHex(m.view_message()), '0x01', 'view_messages result is wrong');
+
+  strictEqual(u8aToHex(m.view_messages()), '0x01', 'view_messages ❌');
   console.log('view_message ✅');
 });
